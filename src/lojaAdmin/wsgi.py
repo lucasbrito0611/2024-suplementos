1ab0425f4aeda58b19dc28d76ab195e65da95c40
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-11-20 22:44:56 -0300

    config

--- src/lojaAdmin/wsgi.py
+++ src/lojaAdmin/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lojaAdmin.settings')
 
