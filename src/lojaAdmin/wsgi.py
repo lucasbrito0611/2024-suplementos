d6b5c3ed9b74d746b5e02123c607815bae6d7123
lucasbrito0611 <lucasbrito2006@hotmail.com>
2024-11-20 22:31:39 -0300

    config

--- src/lojaAdmin/wsgi.py
+++ src/lojaAdmin/wsgi.py
@@ -6,10 +6,12 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
-
+import sys
 import os
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lojaAdmin.settings')
 
